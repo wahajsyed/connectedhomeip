f14f0897554a8db514808fdf867c51f074537826
Wahaj <wahaj.nauman@gmail.com>
2020-09-16 23:19:11 -0700

    Enable serial communication and button actuation over uart for nordic light app

--- examples/lighting-app/nrf5/main/main.cpp
+++ examples/lighting-app/nrf5/main/main.cpp
@@ -21,7 +21,9 @@
 #include <stdint.h>
 
 #include "boards.h"
+#include "nrf.h"
 #include "nrf_delay.h"
+#include "nrf_drv_usbd.h"
 #include "nrf_log.h"
 #ifdef SOFTDEVICE_PRESENT
 #include "nrf_sdh.h"
@@ -65,6 +67,20 @@ extern "C" {
 #include <platform/CHIPDeviceLayer.h>
 #include <support/logging/CHIPLogging.h>
 #endif // CHIP_ENABLE_OPENTHREAD
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "app_error.h"
+#include "app_usbd.h"
+#include "app_usbd_cdc_acm.h"
+#include "app_usbd_core.h"
+#include "app_usbd_serial_num.h"
+#include "app_usbd_string_desc.h"
+#include "app_util.h"
+#ifdef __cplusplus
+}
+#endif
 
 #include "chipinit.h"
 
@@ -128,6 +144,129 @@ extern "C" void JLINK_MONITOR_OnEnter(void) {}
 extern "C" void JLINK_MONITOR_OnPoll(void) {}
 
 #endif // JLINK_MMD
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static void cdc_acm_user_ev_handler(app_usbd_class_inst_t const * p_inst, app_usbd_cdc_acm_user_event_t event);
+
+#define CDC_ACM_COMM_INTERFACE 0
+#define CDC_ACM_COMM_EPIN NRF_DRV_USBD_EPIN2
+
+#define CDC_ACM_DATA_INTERFACE 1
+#define CDC_ACM_DATA_EPIN NRF_DRV_USBD_EPIN1
+#define CDC_ACM_DATA_EPOUT NRF_DRV_USBD_EPOUT1
+
+/**
+ * @brief CDC_ACM class instance
+ * */
+APP_USBD_CDC_ACM_GLOBAL_DEF(m_app_cdc_acm, cdc_acm_user_ev_handler, CDC_ACM_COMM_INTERFACE, CDC_ACM_DATA_INTERFACE,
+                            CDC_ACM_COMM_EPIN, CDC_ACM_DATA_EPIN, CDC_ACM_DATA_EPOUT, APP_USBD_CDC_COMM_PROTOCOL_AT_V250);
+
+// Max read-size
+#define READ_SIZE 1
+
+static char m_rx_buffer[READ_SIZE];
+static char m_tx_buffer[NRF_DRV_USBD_EPSIZE];
+static bool m_send_flag = 0;
+static bool m_ok_flag   = 0;
+/**
+ * @brief User event handler @ref app_usbd_cdc_acm_user_ev_handler_t (headphones)
+ * */
+static void cdc_acm_user_ev_handler(app_usbd_class_inst_t const * p_inst, app_usbd_cdc_acm_user_event_t event)
+{
+    app_usbd_cdc_acm_t const * p_cdc_acm = app_usbd_cdc_acm_class_get(p_inst);
+    static bool state                    = false;
+
+    switch (event)
+    {
+    case APP_USBD_CDC_ACM_USER_EVT_PORT_OPEN: {
+        // bsp_board_led_on(LED_CDC_ACM_OPEN);
+
+        /*Setup first transfer*/
+        ret_code_t ret = app_usbd_cdc_acm_read(&m_app_cdc_acm, m_rx_buffer, READ_SIZE);
+        UNUSED_VARIABLE(ret);
+        break;
+    }
+    case APP_USBD_CDC_ACM_USER_EVT_PORT_CLOSE:
+        // bsp_board_led_off(LED_CDC_ACM_OPEN);
+        break;
+    case APP_USBD_CDC_ACM_USER_EVT_TX_DONE:
+        break;
+    case APP_USBD_CDC_ACM_USER_EVT_RX_DONE: {
+        ret_code_t ret;
+        m_ok_flag = 0;
+        do
+        {
+            /*Get amount of data transfered*/
+            size_t size = app_usbd_cdc_acm_rx_size(p_cdc_acm);
+            NRF_LOG_INFO("RX: size: %lu char: %c", size, m_rx_buffer[0]);
+            if (m_rx_buffer[0] == 'R')
+            {
+                m_ok_flag = 1;
+            }
+            else if (m_rx_buffer[0] == 'B')
+            {
+                LightingManager::Action_t action = (state == false) ? LightingManager::ON_ACTION : LightingManager::OFF_ACTION;
+                GetAppTask().PostLightingActionRequest(action);
+                state     = !state;
+                m_ok_flag = 1;
+            }
+            /* Fetch data until internal buffer is empty */
+
+            ret = app_usbd_cdc_acm_read(&m_app_cdc_acm, m_rx_buffer, size);
+        } while (ret == NRF_SUCCESS);
+        m_send_flag = 1;
+        break;
+    }
+    default:
+        break;
+    }
+}
+
+void usb_setup(void)
+{
+    ret_code_t ret;
+    static const app_usbd_config_t usbd_config = {};
+    app_usbd_serial_num_generate();
+    ret = app_usbd_init(&usbd_config);
+    APP_ERROR_CHECK(ret);
+    NRF_LOG_INFO("USB serial example started.");
+
+    app_usbd_class_inst_t const * class_cdc_acm = app_usbd_cdc_acm_class_inst_get(&m_app_cdc_acm);
+    ret                                         = app_usbd_class_append(class_cdc_acm);
+    APP_ERROR_CHECK(ret);
+    app_usbd_enable();
+    app_usbd_start();
+}
+
+// Actuation Server Task
+void actuation_server(void *)
+{
+    usb_setup();
+    while (true)
+    {
+        while (app_usbd_event_queue_process())
+        {
+            /* Nothing to do */
+        }
+
+        if (m_send_flag)
+        {
+            size_t size    = sprintf(m_tx_buffer, m_ok_flag == 1 ? "OK\r\n" : "NA\r\n");
+            ret_code_t ret = app_usbd_cdc_acm_write(&m_app_cdc_acm, m_tx_buffer, size);
+            APP_ERROR_CHECK(ret);
+            m_send_flag = 0;
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(50));
+    }
+}
+
+#ifdef __cplusplus
+}
+#endif
 
 // ================================================================================
 // Main Code
@@ -240,6 +379,7 @@ int main(void)
 
     NRF_LOG_INFO("Starting FreeRTOS scheduler");
 
+    xTaskCreate(actuation_server, "Actuation", 1024, NULL, 3, NULL);
     /* Start FreeRTOS scheduler. */
     vTaskStartScheduler();
 
